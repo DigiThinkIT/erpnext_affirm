68fdf182c84137d6d4e77ea128424888550ce5ba
Neil Lasrado <neil@digithinkit.com>
2018-02-23 14:57:32 +0530

    App Init

---
+++ README.md
@@ -0,0 +1,7 @@
+## ERPNext Affirm Integration
+
+Affirm (https://www.affirm.com) Integration for ERPNext
+
+#### License
+
+GPL
---
+++ erpnext_affirm/config/desktop.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"module_name": "ERPNext Affirm Integration",
+			"color": "blue",
+			"icon": "octicon octicon-radio-tower",
+			"type": "module",
+			"label": _("ERPNext Affirm Integration")
+		}
+	]
