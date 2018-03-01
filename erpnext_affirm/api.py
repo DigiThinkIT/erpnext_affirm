3cf043fea019ad63902da30f149aade7ed270057
Neil Lasrado <neil@digithinkit.com>
2018-03-01 19:52:01 +0530

    Fixes for Affirm Payment Sucess

---
+++ erpnext_affirm/api.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2018, Neil Lasrado and contributors
+# For license information, please see license.txt
+
+from __future__ import unicode_literals
+import frappe
+
+@frappe.whitelist()
+def collect_charge():
+
+# -*- coding: utf-8 -*-
+# Copyright (c) 2018, Neil Lasrado and contributors
+# For license information, please see license.txt
+
+from __future__ import unicode_literals
+import frappe
+
+@frappe.whitelist()
+def collect_charge():
+
--- erpnext_affirm/www/affirm_success.py
+++ erpnext_affirm/www/affirm_success.py
@@ -12,17 +12,17 @@ def get_context(context):
 	if token:
 		authorization_response = requests.post("https://sandbox.affirm.com/api/v2/charges", auth=HTTPBasicAuth(affirm_settings.public_api_key, affirm_settings.private_api_key), json={"checkout_token": token})
 		affirm_data = authorization_response.json()
-		if affirm_data.get("status_code") == 200:
-			frappe.db.set_value("Quotation", affirm_data.get('order_id'), "affirm_id", affirm_data.get('id'))
-			frappe.db.commit()
+		if affirm_data:
 			quotation = frappe.get_doc("Quotation", affirm_data.get('order_id'))
 			quotation.flags.ignore_permissions = 1
+			quotation.affirm_id = affirm_data.get('id')
 			quotation.run_method("on_payment_authorized", "Authorized")
 			quotation.submit()
+
 			sales_order = frappe.get_doc(_make_sales_order(quotation.name, ignore_permissions=True))
 			sales_order.flags.ignore_permissions = 1
-			sales_order.save()
 			sales_order.submit()
+
 			redirect_url = '/integrations/payment-success'
 
 	frappe.local.response["type"] = "redirect"
